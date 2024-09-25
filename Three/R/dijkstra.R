9f87a63dd3122c13b8b6456f49a6ec876333ac9c
Quanlin Ren <quanlin9502@gmail.com>
2024-09-25 17:23:25 +0200

    add description of algoritm

--- Three/R/dijkstra.R
+++ Three/R/dijkstra.R
@@ -13,10 +13,10 @@
 #'          if the initial node is not in the graph.
 #' @references See [Dijkstra's Algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
 #' @examples
-#' graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#' wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
 #'           v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
 #'           w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-#' wiki_dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 1)
 
 dijkstra <- function(wiki_graph, init_node) {
   if (!init_node %in% c(wiki_graph$v1, wiki_graph$v2)) {
